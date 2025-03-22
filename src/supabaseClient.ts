647e73e140cfad3b4f795a5013c4414a0a6b6d34
medichecker <usemedichecker@gmail.com>
2025-03-22 13:53:26 -0700

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-  throw new Error('Supabase URL or anon key is missing');
-}
+// if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+//   throw new Error('Supabase URL or anon key is missing');
+// }
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl ="https://eifubbnljqheepsxdxqg.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpZnViYm5sanFoZWVwc3hkeHFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNjQxMjIsImV4cCI6MjA1Nzc0MDEyMn0.L5l2yEjZU3HLHiouiksBSBHPxjVYY4mxqcOsUkG2yE8";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
